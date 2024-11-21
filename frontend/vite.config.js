9ebbd213893fd95c184ad777ead48b12258eb314
Ayush <ayushpanchal005@gmail.com>
2024-11-21 18:52:53 +0530

    init

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { config as dotenvConfig } from 'dotenv';
+
+// Load .env variables
+dotenvConfig();
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+});
